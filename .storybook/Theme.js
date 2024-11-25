575435705876bfb009b064abdeb4ef910e65b9be
bendarte <osman.stockholm@gmail.com>
2024-11-25 22:55:41 +0100

    fix: 🐛 Fix storybook Theme setup

--- .storybook/Theme.js
+++ .storybook/Theme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'dark',
-  brandTitle: 'My custom Storybook',
+  brandTitle: 'Ben custom Storybook',
   brandUrl: 'https://example.com',
   brandImage: 'https://ui-avatars.com/api/?name=Component+Library&size=150&background=0D8ABC&color=fff',
   brandTarget: '_self',
